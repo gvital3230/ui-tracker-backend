4f5b7f050cb435392f551de3e86e1c7bacbf5e20
Vitalii Goncharov <1c.audit@gmail.com>
2021-09-06 17:57:36 +0300

    add simple websocket server

--- main.go
+++ main.go
@@ -2,11 +2,36 @@ package main
 
 import (
 	"fmt"
+	"github.com/gorilla/websocket"
 	"html"
 	"log"
 	"net/http"
 	"os"
 )
+
+var upgrader = websocket.Upgrader{} // use default options
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Print("upgrade:", err)
+		return
+	}
+	defer c.Close()
+	for {
+		mt, message, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			break
+		}
+		log.Printf("recv: %s", message)
+		err = c.WriteMessage(mt, message)
+		if err != nil {
+			log.Println("write:", err)
+			break
+		}
+	}
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -17,6 +42,7 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 	})
+	http.HandleFunc("/echo", echo)
 
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
